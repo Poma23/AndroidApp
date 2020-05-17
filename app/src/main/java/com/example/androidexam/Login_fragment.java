20cc179149200cb2db15241d6ed785df943a8f03
Poma23 <43575195+Poma23@users.noreply.github.com>
2020-05-17 23:51:02 +0200

    deleted unused pages

--- app/src/main/java/com/example/androidexam/Login_fragment.java
+++ app/src/main/java/com/example/androidexam/Login_fragment.java
@@ -2,38 +2,36 @@ package com.example.androidexam;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class Login_fragment  extends Fragment {
+import static android.app.Activity.RESULT_OK;
+
+public class Login_fragment extends Fragment {
 
 
     private FirebaseAuth _mFirebaseAuth;
     private FirebaseUser user;
     private FirebaseAuth.AuthStateListener _mAuthListener;
-    public static final int RC_SIGN_IN= 1;
-
-
+    public static final int RC_SIGN_IN = 10;
 
     // Choose authentication providers
-    List< AuthUI.IdpConfig > providers= Arrays.asList(
+    List< AuthUI.IdpConfig > providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.PhoneBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build()
     );
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,93 +39,40 @@ public class Login_fragment  extends Fragment {
         _mFirebaseAuth = FirebaseAuth.getInstance();
         user = _mFirebaseAuth.getCurrentUser();
 
-        _mAuthListener = new FirebaseAuth.AuthStateListener(){
-            @Override
+        if (user != null) {
+            Toast.makeText(getActivity(), "welcome, You are already logged in",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getActivity(), MainActivity.class));
+        } else {
+            // Create and launch sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance().
+                            createSignInIntentBuilder().
+                            setAvailableProviders(providers).setIsSmartLockEnabled(false).
+                            build(), RC_SIGN_IN);
+        }
 
-            public void onAuthStateChanged(FirebaseAuth firebaseAuth){
-                // check if  the user is signed in
-                if( user != null){
-                    Toast.makeText(getContext(),"You are already logged in",
-                            Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(getContext(), MainActivity.class));
-                }else{
-                    // Create and launch sign-in intent
-                    startActivityForResult(
-                            AuthUI.getInstance().
-                                    createSignInIntentBuilder().
-                                    setAvailableProviders(providers).setIsSmartLockEnabled(false).
-                                    build(), RC_SIGN_IN);
-
-                }
-
-            }
-        };
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        _mFirebaseAuth.addAuthStateListener(_mAuthListener);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        _mFirebaseAuth.removeAuthStateListener(_mAuthListener);
-    }
-
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        IdpResponse response = IdpResponse.fromResultIntent(data);
         if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-        //    if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                startActivity(new Intent(getContext(), Home_fragment.class));
-                Toast.makeText(getContext(),"You are now signed in",
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(getContext(), "You are now signed in",
                         Toast.LENGTH_LONG).show();
-                // ...
-        //    } else {
-                // Sign in failed.
-        //        startActivity(new Intent(getActivity(), Login_fragment.class));
-        //        Toast.makeText(getContext(),"You are now signed in failed",
-        //                Toast.LENGTH_LONG).show();
-          //  }
+                startActivity(new Intent(getActivity(), MainActivity.class));
+
+            } else {
+                if (response == null) {
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    return;
+                }
+            }
         }
     }
-
-    public void signOut(View view) {
-        AuthUI.getInstance().signOut(getContext()).addOnCompleteListener(
-                new OnCompleteListener< Void >() {
-                    @Override
-                    public void onComplete(@NonNull Task< Void > task) {
-                        Toast.makeText(getContext(),"You are now signed out",
-                                Toast.LENGTH_LONG).show();
-                        //finish();
-                    }
-                }
-        );
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
