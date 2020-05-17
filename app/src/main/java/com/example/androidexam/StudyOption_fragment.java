57b6762fdedb97c47c9f235ddd3b8a2e69513f11
Poma23 <43575195+Poma23@users.noreply.github.com>
2020-05-17 22:49:12 +0200

    deleted 4 pages from study options
    put text insted of image views

--- app/src/main/java/com/example/androidexam/StudyOption_fragment.java
+++ app/src/main/java/com/example/androidexam/StudyOption_fragment.java
@@ -6,65 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class StudyOption_fragment extends Fragment
 {
-    ImageButton uni, uniCollage, art, maritime;
     Button list;
-
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_study_options, container, false);
+        list= view.findViewById(R.id.button_UniList);
 
-        view.findViewById(R.id.imageButton_uni).setOnClickListener(myListener);
-        view.findViewById(R.id.imageButton_university_colegae).setOnClickListener(myListener);
-        view.findViewById(R.id.imageButtonFineArt).setOnClickListener(myListener);
-        view.findViewById(R.id.imageButtonMaritimeEducation).setOnClickListener(myListener);
-        view.findViewById(R.id.button_UniList).setOnClickListener(myListener);
-
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent5= new Intent(getActivity(),UniList.class);
+                startActivity(intent5);
+            }
+        });
         return view;
     }
-
-    private final View.OnClickListener myListener = new View.OnClickListener()
-    {
-        @Override
-        public void onClick(View v)
-        {
-             switch (v.getId())
-             {
-              /*  case R.id.imageButton_uni:
-                     Intent intent= new Intent(getActivity(), University.class);
-                     startActivity(intent);
-                     break; */
-
-                 case R.id.imageButton_university_colegae:
-                     Intent intent2= new Intent(getActivity(), University_collage.class);
-                     startActivity(intent2);
-                     break;
-
-                 case R.id.imageButtonFineArt:
-                     Intent intent3= new Intent(getActivity(), Artistic.class);
-                     startActivity(intent3);
-                     break;
-                 case R.id.imageButtonMaritimeEducation:
-                     Intent intent4= new Intent(getActivity(), Maritime.class);
-                     startActivity(intent4);
-                     break;
-                 case R.id.button_UniList:
-                     Intent intent5= new Intent(getActivity(),UniList.class);
-                     startActivity(intent5);
-             }
-
-        }
-    };
 
 
 }
