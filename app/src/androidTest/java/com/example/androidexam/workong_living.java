35dadb83f999a082719c3707103689fdb88cca7c
Poma23 <43575195+Poma23@users.noreply.github.com>
2020-06-12 11:14:11 +0200

    TTest-seperated  navigation items

---
+++ app/src/androidTest/java/com/example/androidexam/workong_living.java
@@ -0,0 +1,128 @@
+package com.example.androidexam;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class workong_living {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void workong_living() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open drawerK"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navigationView),
+                                        0)),
+                        3),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction tabView = onView(
+                allOf(withContentDescription("Work"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tablayout),
+                                        0),
+                                1),
+                        isDisplayed()));
+        tabView.perform(click());
+
+        ViewInteraction tabView2 = onView(
+                allOf(withContentDescription("Map"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tablayout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        tabView2.perform(click());
+
+        ViewInteraction tabView3 = onView(
+                allOf(withContentDescription("Life"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tablayout),
+                                        0),
+                                0),
+                        isDisplayed()));
+        tabView3.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.imgVisa),
+                        childAtPosition(
+                                withParent(withId(R.id.viewPager)),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        pressBack();
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withId(R.id.imgHealth),
+                        childAtPosition(
+                                withParent(withId(R.id.viewPager)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
